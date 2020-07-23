441a3e85ea33b0c8178ac35be49fc43c2f51b377
Mikkel Aagaard <maa@simplesite.com>
2020-07-23 13:45:51 +0200

    change name to holopedia

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -73,8 +73,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 //TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "holopediacoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101d2e27da7fcde77930a16047bf3d61fbe73a2f3e8f495c7929c6c9ceeb69024a7";
+const char     CRYPTONOTE_NAME[]                             = "holopedia";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
